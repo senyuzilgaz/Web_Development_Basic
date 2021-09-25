d00d66765eb32fb5ea05e17f3170dfb4fa180a92
ilgazsenyuz <=ilgazsenyuz@hotmail.com>
2021-09-25 14:54:13 +0300

    edit

--- Agriculture_Project/Default.aspx.designer.vb
+++ Agriculture_Project/Default.aspx.designer.vb
@@ -12,4 +12,13 @@ Option Explicit On
 
 
 Partial Public Class _Default
+
+    '''<summary>
+    '''Repeater1 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents Repeater1 As Global.System.Web.UI.WebControls.Repeater
 End Class
